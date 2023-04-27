f35c657f00af7572fefb62c0a60995b66d430549
Ayk <brutalmind99@gmail.com>
2023-04-27 15:08:31 +0800

    Initial Commit

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,28 @@
+// alert("Hello from JS file");
+
+//log num
+console.log(100);
+
+//log string
+console.log("Hello World");
+
+//log multiple values
+console.log(20, "Hello", true);
+
+// log a variable
+const x = 100;
+console.log(x);
+
+// console error & warning
+console.error("Alert");
+console.warn("Warning");
+
+// log object as a table
+console.table({ name: "Ayk", email: "123@gmail.com" });
+
+// Group Console Commands
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
