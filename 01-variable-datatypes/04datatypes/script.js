7ff0130cdec02ef7806a1b3259218eb04cd711a1
Ayk <brutalmind99@gmail.com>
2023-04-28 13:45:43 +0800

    Initial Commit

---
+++ 01-variable-datatypes/04datatypes/script.js
@@ -0,0 +1,44 @@
+// String
+const firstName = "Ayk";
+// console.log(firstName);
+
+// console.log(output, typeof output);
+
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// null
+const aptNumber = null;
+
+// undefined
+// let score;
+// console.log(score);
+
+const score = undefined;
+
+// symbol
+const id = Symbol("id");
+
+// BigInt
+const n = 314159265358979323846n;
+
+// Reference Types
+const numbers = [1, 2, 3, 4];
+const person = {
+  fname: "Ayk",
+};
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+// const output = sayHello;
+// console.log(output, typeof output);
+
+const myString = "developer";
+const myNewString = myString.charAt(0).toUpperCase() + myString.slice(1);
+
+console.log(myNewString);
---
+++ 01-variable-datatypes/13challenge2-html/script.js
@@ -0,0 +1,14 @@
+let x = Math.floor(Math.random() * 100) + 1;
+let y = Math.floor(Math.random() * 50) + 1;
+
+const sumOutput = x + y;
+const differenceOutput = x - y;
+const productOutput = x * y;
+const quotientOutput = x / y;
+const rmOutput = x % 7;
+
+console.log(`${x} + ${y} = ${sumOutput}`);
+console.log(`${x} + ${y} = ${differenceOutput}`);
+console.log(`${x} + ${y} = ${productOutput}`);
+console.log(`${x} + ${y} = ${quotientOutput}`);
+console.log(`${x} + ${y} = ${rmOutput}`);
